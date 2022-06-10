cd5616a16cff79abd7c4e0a70d1806dbfa72b3ab
Daniel Yang <daniel.yang17@gmail.com>
2022-06-10 00:56:25 -0400

    __init__ for happy import

---
+++ __init__.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+# dxy: welcome to my pile of hacks
+sys.path.append(os.path.expanduser("~/code/hands/frankmocap"))
+
+# pose2d_estimator_path = "~/code/hands/frankmocap/detectors/body_pose_estimator"
+# sys.path.append(os.path.expanduser(pose2d_estimator_path))
+
+# hand_object_detector_path = '~/code/hands/frankmocap/detectors/hand_object_detector'
+# sys.path.append(os.path.expanduser(hand_object_detector_path))
+
+# import bodymocap
+# import demo
+# import detectors
+# import handmocap
+# import integration
+# import mocap_utils
+# import renderer
